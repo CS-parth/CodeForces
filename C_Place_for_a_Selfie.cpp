9cf02f316895be0a18f3e1854871d8df8db9ab4d
CS-parth <parth.vijy@gmail.com>
2023-10-31 22:11:57 +0530

    added

---
+++ C_Place_for_a_Selfie.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+#define int long long 
+#define input(a) for (int &x : a) cin >> x;
+
+void init_1(){
+    int n,m;
+    cin >> n >> m;
+    set<int> st;
+    for(int i =0;i<n;i++){
+        int ok;
+        cin >> ok;
+        st.insert(ok);
+    }
+    for(int i = 0; i < m;i++){
+        int a,b,c;
+        cin >> a >> b >> c;
+        // take upper bound 
+        auto it = st.lower_bound(b);
+        // auto it1 = --it2;
+        int op1 = (it != st.end()) ? *it : 1e18; // == ya fir > 
+        int op2 = (it != st.begin()) ? *(--it) : 1e18; // less than key
+        int temp = min(abs(op1-b),abs(op2-b));
+        if((double)(temp*temp)/4 < a*c){
+            // answer exists 
+            cout << "YES" << "\n";
+            if(temp == abs(op1-b)){
+                cout << op1 << "\n";
+            }else{
+                cout << op2 << "\n";
+            }
+        }else{
+            cout << "NO" << "\n";
+        }
+    }
+}
+
+signed main() {
+std::ios::sync_with_stdio(false);
+#ifndef ONLINE_JUDGE
+	freopen("Error.txt", "w", stderr);
+#endif
+    int T;
+    cin >> T;
+    for(int k = 0 ; k < T ; k++){
+        init_1();  
+        cout << "\n";
+    }
+}
--- MIN_To_MAX.cpp
+++ MIN_To_MAX.cpp
@@ -1,19 +1,13 @@
-#include <bits/stdc++.h>
-#define loop(i,n) for(int i = 0 ; i < n ; i++)
-using namespace std;
-int main (){
-    int T;
-    cin >> T;
-    for(int i = 0 ; i < T ; i++){
-        multiset<int> st;
-        int x;
-        cin >> x;
-        while(x--){
-            int ok;
-            cin >> ok;
-            st.insert(ok);
+class Solution {
+public:
+    int dp[100001];
+    long long minIncrementOperations(vector<int>& nums, int k) {
+        // memset(dp,-1,sizeof(dp));
+        for(int i = 0; i < nums.size()-1;i++){
+            // make it greater than k 
+            if(int i 
+                
+            }
         }
-        st.erase(*st.begin());
-        cout << st.size() << "\n";
     }
-}
+};
