3e7d20afc6d660dd40190e07814bd6c41f3179ee
CS-parth <parth.vijy@gmail.com>
2024-10-13 22:25:43 +0530

    Added more questions

--- F/F_Selling_a_Menagerie.cpp
+++ F/F_Selling_a_Menagerie.cpp
@@ -1,6 +1,7 @@
 /*Jai Shri Ram*/
 /*Jai Bajrang Bali*/
 /*Hare krishna hare krishna krishna krishna hare hare hare rama hare rama rama hare hare*/
+/*ॐ कृष्णाय वासुदेवाय हरये परमात्मने प्रणत क्लेशनाशाय गोविंदाय नमो नमः*/
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -16,62 +17,87 @@ using namespace std;
 #define input(a) for (auto &x : a) cin >> x;
 #define print(a) for (auto &x : a) cout << x << " ";cout << "\n";
 
+
 void init_1(){
+    // First do leaf cutting 
     int n;
     cin >> n;
-    vector<int> affraid(n);
-    vector<int> cost(n);
-    vector<int> in(n);
-    vector<int> out(n,1);
-    input(affraid);
-    input(cost);
-    for(int i = 0;i < n;i++) affraid[i]--;
+    vector<int> a(n);
+    vector<int> b(n);
+    input(a);
+    input(b);
+    vector<int> adj[n];
+    vector<int> indeg(n);
     for(int i = 0;i < n;i++){
-        in[affraid[i]]++;
+        adj[i].push_back(--a[i]);
+        indeg[a[i]]++;
     }
+
     queue<int> q;
-    // place all the nodes with in[i] = 0
     for(int i = 0;i < n;i++){
-        if(in[i] == 0){
+        if(indeg[i] == 0){
             q.push(i);
         }
     }
-    vector<int> ans;
+
+    vector<bool> vis(n,false);
+    
     while(!q.empty()){
         int node = q.front();
         q.pop();
-        ans.push_back(node);
-        // what it out
-        int affraidOf = affraid[node];
-        in[affraidOf]--; // removing node
-        if(in[affraidOf] == 0) q.push(affraidOf);
+        vis[node] = true;
+        cout << node+1 << " ";
+        for(auto &it : adj[node]){
+            indeg[it]--; // while going from node to it the indeg will reduce
+            if(indeg[it] == 0) q.push(it);
+        }
     }
-    int start = 0;
+
     for(int i = 0;i < n;i++){
-        if(in[i] != 0){
-            start = i;
-            break;
+        if(vis[i] == false){
+            int val = -1;
+            int mn = 1e15;
+            q.push(i);
+            vector<int> v;
+            while(!q.empty()){
+                int node = q.front();
+                q.pop();
+                if(vis[node]) continue;
+                vis[node] = true;
+                v.push_back(node);
+                if(b[node] < mn){
+                    mn = b[node];
+                    val = node;
+                }
+                for(auto &it : adj[node]){
+                    q.push(it);
+                }
+            }
+            // print(v);
+            // end at node
+            int index = -1;
+            for(int i = 0;i < v.size();i++){
+                if(v[i] == val){
+                    index = i;
+                    break;
+                }
+            }
+            index++;
+            index = (index%v.size());
+            int j = index;
+            // cout << '\n';
+            // cout << j << " " << index << " ";
+            do{
+                cout << v[j]+1 << " ";
+                j++;
+                j = (j%v.size());
+            }while(index!=j);
         }
     }
-    // cout << start << '\n';
-    int mnStart = start;
-    int temp = start;
-    do{
-        // next
-        int temp = affraid[temp];
-        if(cost[temp] < cost[mnStart]){
-            mnStart = temp;
-        }
-    }while(temp!=start);
-    start = mnStart;
-    temp = start;
-    do{
-        ans.push_back(temp);
-        temp = affraid[temp];
-    }while (temp!=start);
-    for(int i = 0;i < n;i++) ans[i]+=1;
-    print(ans);
-}
+
+    cout << "\n";
+}  
+
 
 signed main() {
 std::ios::sync_with_stdio(false);
